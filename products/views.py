725e86dd70d80fbe1a3cc72f6ae080f713a4443f
Johnathonanon <jconry21@gmail.com>
2022-06-23 14:52:21 +0000

    created product details page, and functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 """ Products views file """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -14,3 +14,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ View showing individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
