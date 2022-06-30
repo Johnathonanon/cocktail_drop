0f365aafff47813e1eb0d33b0a963028a066e016
Johnathonanon <jconry21@gmail.com>
2022-06-30 15:39:43 +0000

    add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KxYVNK1fke01IYS97o1sffIIXhx9633m2cImvP2vCU8Fg5L9N30WHnEJBENv3HrPRaWYHEOenIaK3L3rZWERuJ400Y2CZEdoj',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
