734a8cb0dcc46cc8c9b4e872eeee740590780d45
Johnathonanon <jconry21@gmail.com>
2022-07-07 16:16:23 +0000

    create review and rating models

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 """ Products app models """
 
 from django.db import models
+from django.db.models import Avg, Count
+from profiles.models import UserProfile
 
 
 class Category(models.Model):
@@ -29,9 +31,51 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def averagereview(self):
+        """ calculates average review rating """
+        review = Review.objects.filter(
+            product=self).aggregate(average=Avg('rating'))
+        avg = 0
+        if review["average"] is not None:
+            avg = float(review["average"])
+        return avg
+
+    def countreview(self):
+        """ counts reviews """
+        reviews = Review.objects.filter(
+            product=self).aggregate(count=Count('id'))
+        cnt = 0
+        if reviews["count"] is not None:
+            cnt = int(reviews["count"])
+        return cnt
 
     def __str__(self):
         return str(self.name)
+
+
+class Rating(models.Model):
+    """ product rating model """
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='ratings')
+    product = models.ForeignKey(Product, models.CASCADE, null=True, blank=True)
+    rating = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'This rating is for {self.product}'
+
+
+class Review(models.Model):
+    """ product review model """
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='reviews')
+    product = models.ForeignKey(Product, models.CASCADE, null=True, blank=True)
+    heading = models.CharField(max_length=100, null=True, blank=True)
+    comment = models.TextField(max_length=250, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.heading)
