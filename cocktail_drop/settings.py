c61d5478431b5e6057f2f28f8b96ec208d45a551
Johnathonanon <jconry21@gmail.com>
2022-07-10 17:25:40 +0000

    fix media bug

--- cocktail_drop/settings.py
+++ cocktail_drop/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # allauth req
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
             'builtins': [
